3a1ecc9d377918b21823f019e04796a38582fcbb
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2023-12-26 20:31:39 +0530

    base changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/Portfolio-Sharjun-Hussain/',
+  base: '/Portfolio-Sharjun-Hussain/',
 })
