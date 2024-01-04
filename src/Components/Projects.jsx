85056811fabbc7da2ca4df32c13b1f6ea66ba0ea
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-04 19:02:14 +0530

    Project Hover Section Animation

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,6 @@
 import { Card, Col, Container, Row } from "react-bootstrap";
 import Bookmarking from "../assets/Icons/Bookmarking.gif";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
@@ -18,9 +19,8 @@ const Projects = () => {
         </Col>
       </Row>
 
-     
       <Row className="mt-4">
-      <Col md={4} xs={12}>
+        <Col md={4} xs={12}>
           <div className="Project-Card ">
             <Card>
               <Card.Header className="d-flex  justify-content-start ">
@@ -89,66 +89,14 @@ const Projects = () => {
                 />
               </Card.Header>
               <Card.Body className=" text-white ">
-                <div
-                  className="px-4 text-white"
-                  style={{ background: "transparent", fontSize: "22px" }}
-                >
-                  EventSpot Hall Booking
-                </div>
-                <div
-                  className="px-4 text-white"
-                  style={{
-                    background: "transparent",
-                    fontSize: "17px",
-                    fontWeight: 300,
-                    opacity: 0.8,
-                  }}
-                >
-                  {" "}
-                  Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Minima, et. Dolore maiores magnam voluptatum molestias quos
-                  eius expedita quae fugit est reprehenderit veniam veritatis
-                  similique, quia eos beatae. Illo, maiores!
-                </div>
-              </Card.Body>
-              <Card.Footer>
-                <div
-                  className="px-4 text-white mb-3"
-                  style={{
-                    background: "transparent",
-                    fontSize: "13px",
-                    fontWeight: 300,
-                    opacity: 0.6,
-                  }}
-                >
-                  Mern Stack | Bootstrap | Netlify
-                </div>
-              </Card.Footer>
-            </Card>
-          </div>
-        </Col>
-
-        <Col md={4} xs={12}>
-          <div className="Project-Card">
-            <Card>
-              <Card.Header className="d-flex  justify-content-start ">
-                <img
-                  src={Bookmarking}
-                  width={45}
-                  style={{
-                    background: "transparent",
-                    marginLeft: "13px",
-                    marginTop: "10px",
-                  }}
-                />
-              </Card.Header>
-              <Card.Body className=" text-white ">
-                <div
-                  className="px-4 text-white"
-                  style={{ background: "transparent", fontSize: "22px" }}
-                >
-                  EventSpot Hall Booking
-                </div>
+               
+                  <div
+                    className="px-4 text-white"
+                    style={{ background: "transparent", fontSize: "22px" }}
+                  >
+                    <Link to="https://github.com/Sharjun-Hussain/EventnSpot-Web-Application">EventSpot Hall Booking</Link> 
+                 </div>
+               
                 <div
                   className="px-4 text-white"
                   style={{
@@ -344,13 +292,60 @@ const Projects = () => {
           </div>
         </Col>
 
+        <Col md={4} xs={12}>
+          <div className="Project-Card">
+            <Card>
+              <Card.Header className="d-flex  justify-content-start ">
+                <img
+                  src={Bookmarking}
+                  width={45}
+                  style={{
+                    background: "transparent",
+                    marginLeft: "13px",
+                    marginTop: "10px",
+                  }}
+                />
+              </Card.Header>
+              <Card.Body className=" text-white ">
+                <div
+                  className="px-4 text-white"
+                  style={{ background: "transparent", fontSize: "22px" }}
+                >
+                  EventSpot Hall Booking
+                </div>
+                <div
+                  className="px-4 text-white"
+                  style={{
+                    background: "transparent",
+                    fontSize: "17px",
+                    fontWeight: 300,
+                    opacity: 0.8,
+                  }}
+                >
+                  {" "}
+                  Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Minima, et. Dolore maiores magnam voluptatum molestias quos
+                  eius expedita quae fugit est reprehenderit veniam veritatis
+                  similique, quia eos beatae. Illo, maiores!
+                </div>
+              </Card.Body>
+              <Card.Footer>
+                <div
+                  className="px-4 text-white mb-3"
+                  style={{
+                    background: "transparent",
+                    fontSize: "13px",
+                    fontWeight: 300,
+                    opacity: 0.6,
+                  }}
+                >
+                  Mern Stack | Bootstrap | Netlify
+                </div>
+              </Card.Footer>
+            </Card>
+          </div>
+        </Col>
       </Row>
-       
-     
-
-        
-        
-      
     </Container>
   );
 };
