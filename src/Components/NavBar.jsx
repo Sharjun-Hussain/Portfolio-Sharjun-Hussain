41fc46d3a7f72f00e591ac99f9f1d9b75f10ab39
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-04 17:25:13 +0530

    Removed Icons from Navbar Items

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,8 +3,8 @@ import logo from "../assets/Logo/logo.png";
 
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import HomeIcon from "../assets/Icons/Home.gif";
-import ContactIcon from "../assets/Icons/Contact.gif";
+// import HomeIcon from "../assets/Icons/Home.gif";
+// import ContactIcon from "../assets/Icons/Contact.gif";
 
 import Offcanvas from "react-bootstrap/Offcanvas";
 
@@ -28,31 +28,31 @@ function TopNav() {
               <Nav className=" d-flex  justify-content-end flex-grow-1 pe-3  my-3  py-4">
                 <Nav.Link href="#about" className="px-3 ">
                   {" "}
-                  <img
+                  {/* <img
                     src={HomeIcon}
                     className="me-2"
                     alt="Home Icon"
                     width={25}
-                  />{" "}
+                  />{" "} */}
                   <span className="text-white">01. </span> About
                 </Nav.Link>
                 <Nav.Link href="#projects" className="px-3">
                   {" "}
-                  <img
+                  {/* <img
                     src={HomeIcon}
                     className="me-3"
                     alt="Home Icon"
                     width={25}
-                  />
+                  /> */}
                        <span className="text-white">02. </span> Projects
                 </Nav.Link>
                 <Nav.Link href="#contact" className="px-3  ">
-                  <img
+                  {/* <img
                     src={ContactIcon}
                     className="me-2"
                     alt="Home Icon"
                     width={25}
-                  />{" "}
+                  />{" "} */}
                       <span className="text-white ">03. </span> Contact
                 </Nav.Link>
                 <a className="Resumebutton">Resume</a>
